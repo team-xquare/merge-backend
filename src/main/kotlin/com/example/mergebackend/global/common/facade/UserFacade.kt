f0fdc6d2c07cbdca01c65ef24844e6f04029a815
4mjeo <4mj.eo@dsm.hs.kr>
2024-01-31 18:00:09 +0900

    feat :: UserFacade 추가

---
+++ src/main/kotlin/com/example/mergebackend/global/common/facade/UserFacade.kt
@@ -0,0 +1,14 @@
+package com.example.mergebackend.global.common.facade
+
+import com.example.mergebackend.domain.user.entity.User
+import com.example.mergebackend.domain.user.repository.UserRepository
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+    private val userRepository: UserRepository
+) {
+    fun getCurrentUser(): User = userRepository.findBySchoolGcn(SecurityContextHolder.getContext().authentication.name)
+            ?: throw InvalidTokenException
+}
