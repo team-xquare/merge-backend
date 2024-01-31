b87289a9ace53775a69a513d6b4a49926d21a1be
4mjeo <4mj.eo@dsm.hs.kr>
2024-01-31 12:23:09 +0900

    chore :: project setting

---
+++ src/test/kotlin/com/example/mergebackend/MergeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mergebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MergeBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
