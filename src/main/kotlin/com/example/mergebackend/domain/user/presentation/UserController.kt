5ffe640a592b3bd794b5989dd615eca932936b3c
4mjeo <4mj.eo@dsm.hs.kr>
2024-01-31 18:35:31 +0900

    feat :: UserController 추가

---
+++ src/main/kotlin/com/example/mergebackend/domain/user/presentation/UserController.kt
@@ -0,0 +1,29 @@
+package com.example.mergebackend.domain.user.presentation
+
+import com.example.mergebackend.domain.user.presentation.dto.request.UpdateProfileRequest
+import com.example.mergebackend.domain.user.presentation.dto.response.UserProfileResponse
+import com.example.mergebackend.domain.user.service.UserService
+import jakarta.validation.Valid
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Validated
+@RestController
+@RequestMapping("/user")
+class UserController(
+    private val userService: UserService
+) {
+
+    @GetMapping("/myInfo")
+    fun getProfile(): UserProfileResponse = userService.getProfile()
+
+    @PutMapping("/update")
+    fun updateProfile(
+            @RequestBody @Valid
+            req: UpdateProfileRequest
+    ): UserProfileResponse = userService.updateProfile(req)
+}
