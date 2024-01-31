02e8c22026c1d4054bd317e2da9449fdd361ccdb
4mjeo <4mj.eo@dsm.hs.kr>
2024-01-31 12:23:23 +0900

    chore :: project setting

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    //jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    //security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    //validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+
+    //jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    //kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    //test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/example/mergebackend/MergeBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mergebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MergeBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MergeBackendApplication>(*args)
+}
