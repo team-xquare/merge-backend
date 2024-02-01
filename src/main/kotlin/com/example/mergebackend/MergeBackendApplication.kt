1ce053b78708433cd4884bc7da5c41b1dbba892b
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-02 02:06:13 +0900

    refactor :: ConfigurationPropertiesScan annotation

--- src/main/kotlin/com/example/mergebackend/MergeBackendApplication.kt
+++ src/main/kotlin/com/example/mergebackend/MergeBackendApplication.kt
@@ -1,8 +1,9 @@
 package com.example.mergebackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class MergeBackendApplication
 
