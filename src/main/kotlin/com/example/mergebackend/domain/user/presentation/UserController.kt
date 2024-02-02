bd17307708467e669c0b0fd89c81bfce9d5aa232
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-02 10:47:06 +0900

    refactor :: delete updateProfile logic

--- src/main/kotlin/com/example/mergebackend/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/example/mergebackend/domain/user/presentation/UserController.kt
@@ -1,13 +1,9 @@
 package com.example.mergebackend.domain.user.presentation
 
-import com.example.mergebackend.domain.user.presentation.dto.request.UpdateProfileRequest
 import com.example.mergebackend.domain.user.presentation.dto.response.UserProfileResponse
 import com.example.mergebackend.domain.user.service.UserService
-import jakarta.validation.Valid
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,9 +17,4 @@ class UserController(
     @GetMapping("/myInfo")
     fun getProfile(): UserProfileResponse = userService.getProfile()
 
-    @PutMapping("/update")
-    fun updateProfile(
-            @RequestBody @Valid
-            req: UpdateProfileRequest
-    ): UserProfileResponse = userService.updateProfile(req)
 }
