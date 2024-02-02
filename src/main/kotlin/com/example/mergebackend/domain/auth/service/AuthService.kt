e870b55ec73a6b49af214d78adfb4bcc73ed599a
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-02 10:36:15 +0900

    feat ::  add AuthService

---
+++ src/main/kotlin/com/example/mergebackend/domain/auth/service/AuthService.kt
@@ -0,0 +1,12 @@
+package com.example.mergebackend.domain.auth.service
+
+import com.example.mergebackend.domain.auth.presentation.dto.request.LoginRequest
+import com.example.mergebackend.domain.auth.presentation.dto.request.ReissueRequest
+import com.example.mergebackend.domain.auth.presentation.dto.request.SignUpRequest
+import com.example.mergebackend.domain.auth.presentation.dto.response.TokenResponse
+
+interface AuthService {
+    fun login(req: LoginRequest): TokenResponse
+    fun signup(req: SignUpRequest)
+    fun reissue(req:ReissueRequest): TokenResponse
+}
---
+++ src/main/kotlin/com/example/mergebackend/domain/auth/service/AuthServiceImpl.kt
@@ -0,0 +1,49 @@
+package com.example.mergebackend.domain.auth.service
+
+import com.example.mergebackend.domain.auth.exception.PasswordMismatchedException
+import com.example.mergebackend.domain.auth.presentation.dto.request.LoginRequest
+import com.example.mergebackend.domain.auth.presentation.dto.request.ReissueRequest
+import com.example.mergebackend.domain.auth.presentation.dto.request.SignUpRequest
+import com.example.mergebackend.domain.auth.presentation.dto.response.TokenResponse
+import com.example.mergebackend.domain.user.entity.User
+import com.example.mergebackend.domain.user.exception.UserNotFoundException
+import com.example.mergebackend.domain.user.repository.UserRepository
+import com.example.mergebackend.global.config.jwt.TokenProvider
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class AuthServiceImpl(
+        private val userRepository: UserRepository,
+        private val passwordEncoder: PasswordEncoder,
+        private val tokenProvider: TokenProvider
+): AuthService {
+
+    @Transactional
+    override fun login(req: LoginRequest): TokenResponse {
+        val user = userRepository.findByAccountId(req.accountId!!)
+                ?: throw UserNotFoundException
+
+        if (!passwordEncoder.matches(req.password, user.password)) throw PasswordMismatchedException
+
+        return tokenProvider.receiveToken(user.accountId)
+    }
+
+    @Transactional
+    override fun signup(request: SignUpRequest) {
+
+        userRepository.save(User(
+                studentName = request.studentName!!,
+                accountId = request.accountId!!,
+                password = passwordEncoder.encode(request.password!!),
+                schoolGcn = request.schoolGcn!!,
+                email = request.email!!,
+                github = request.github!!
+        ))
+    }
+
+    @Transactional
+    override fun reissue(req: ReissueRequest): TokenResponse = tokenProvider.reissue(req.refreshToken)
+}
