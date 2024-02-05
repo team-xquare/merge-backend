8463984922628ec998b14114362eef4462fa0115
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-06 02:52:20 +0900

    feat :: add Project request dtos

---
+++ src/main/kotlin/com/example/mergebackend/domain/project/presentation/dto/request/UpdateProjectRequest.kt
@@ -0,0 +1,40 @@
+package com.example.mergebackend.domain.project.presentation.dto.request
+
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Pattern
+
+data class UpdateProjectRequest(
+
+        @field:NotBlank(message = "null이 될 수 없습니다.")
+        val logo: String,
+
+        @field:NotBlank(message = "null이 될 수 없습니다.")
+        val description: String,
+
+        val projectImage: String?,
+
+        @field:Pattern(
+                regexp = "^https://github\\.com/.+",
+                message = "올바른 깃허브 링크가 아닙니다."
+        )
+        val githubUrl: String?,
+
+        @field:Pattern(
+                regexp = "^(http|https)://",
+                message = "올바른 웹주소 형식이 아닙니다."
+        )
+        val webUrl: String?,
+
+        @field:Pattern(
+                regexp = "^https://play\\.google\\.com/store/apps/details\\?id=[a-zA-Z0-9._-]+\$\n",
+                message = "올바른 플레이스토어 주소가 아닙니다."
+        )
+        val playStoreUrl: String?,
+
+        @field:Pattern(
+                regexp = "^https://apps\\.apple\\.com/[a-z]{2}/app/[a-zA-Z0-9._-]+/[a-zA-Z0-9._-]+\$\n",
+                message = "올바른 앱스토어 주소가 아닙니다."
+        )
+        val appStoreUrl: String?
+) {
+}
