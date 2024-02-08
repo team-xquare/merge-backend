4e3f6f0fadb387f0c6f5ca9b61c46e052a4f1f8f
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-08 17:29:23 +0900

    feat :: findByEmail 추가

--- src/main/kotlin/com/example/mergebackend/domain/project/repository/ProjectRepository.kt
+++ src/main/kotlin/com/example/mergebackend/domain/project/repository/ProjectRepository.kt
@@ -1,11 +1,12 @@
 package com.example.mergebackend.domain.project.repository
 
 import com.example.mergebackend.domain.project.entity.Project
+import com.example.mergebackend.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
 interface ProjectRepository: JpaRepository<Project, UUID>{
-
+    fun findByUser(user: User): List<Project>
 }
