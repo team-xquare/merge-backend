87e62dfb38382ad726eadd94897ee69aab49c180
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-08 20:18:21 +0900

    refactor :: restful

--- src/main/kotlin/com/example/mergebackend/domain/project/presentation/ProjectController.kt
+++ src/main/kotlin/com/example/mergebackend/domain/project/presentation/ProjectController.kt
@@ -10,25 +10,16 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
-import javax.validation.Valid
 
 @Validated
 @RestController
 @RequestMapping("/project")
 class ProjectController(
-        private val projectService: ProjectService
+        private val projectService: ProjectService,
+        private val objectMapper: ObjectMapper
 ) {
 
     @PostMapping
@@ -50,8 +41,7 @@ class ProjectController(
             @RequestPart(value = "logo", required = true) logo: MultipartFile,
             @RequestPart("project") project: String
     ): ProjectDetailResponse {
-        val mapper = ObjectMapper()
-        val request: UpdateProjectRequest = mapper.readValue(project, UpdateProjectRequest::class.java)
+        val request: UpdateProjectRequest = objectMapper.readValue(project, UpdateProjectRequest::class.java)
         val finalRequest = request.copy(logo = logo)
         return projectService.update(projectId, finalRequest)
     }
@@ -61,9 +51,9 @@ class ProjectController(
             @RequestParam("id") id: UUID
     ): ProjectDetailResponse = projectService.getDetail(id)
 
-    @GetMapping("/user/{email}")
+    @GetMapping("/user")
     fun getProjectByUser(
-            @PathVariable("email") email: String
+            @RequestParam("email") email: String
     ): List<ProjectListResponse> = projectService.getProject(email)
 
     @GetMapping("/list")
