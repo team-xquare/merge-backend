ea5a8293bf2c374e75133998a6d945301e606998
HyunSu1768 <azxcv1768@gmail.com>
2024-02-14 19:26:44 +0900

    feat :: regex 패턴 추가

--- src/main/kotlin/com/example/mergebackend/domain/deploy/presentation/dto/request/CreateDeployRequest.kt
+++ src/main/kotlin/com/example/mergebackend/domain/deploy/presentation/dto/request/CreateDeployRequest.kt
@@ -3,11 +3,16 @@ package com.example.mergebackend.domain.deploy.presentation.dto.request
 import com.example.mergebackend.domain.deploy.entity.type.ServiceType
 import com.example.mergebackend.domain.deploy.entity.vo.UseDatabase
 import java.util.UUID
+import javax.validation.constraints.Pattern
 
 data class CreateDeployRequest(
     val containerName: String,
     val projectId: UUID,
     val serviceType: ServiceType,
     val useDatabase: UseDatabase,
+    @field:Pattern(
+        regexp = "^https://github.com/[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+",
+        message = "올바른 github 주소가 아닙니다"
+    )
     val githubUrl: String
 )
