81533dcb42dd2c86012f2c58abfc3253bcf9fb4e
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-14 15:28:57 +0900

    refactor :: 필요없는 의존성 제거

--- src/main/kotlin/com/example/mergebackend/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/com/example/mergebackend/domain/user/service/UserServiceImpl.kt
@@ -1,15 +1,11 @@
 package com.example.mergebackend.domain.user.service
 
-import com.example.mergebackend.domain.project.entity.Project
-import com.example.mergebackend.domain.user.entity.User
 import com.example.mergebackend.domain.user.presentation.dto.request.UpdateProfileRequest
 import com.example.mergebackend.domain.user.presentation.dto.response.UserProfileResponse
 import com.example.mergebackend.domain.user.repository.UserRepository
 import com.example.mergebackend.global.common.facade.UserFacade
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
 
 @Service
 @Transactional(readOnly = true)
