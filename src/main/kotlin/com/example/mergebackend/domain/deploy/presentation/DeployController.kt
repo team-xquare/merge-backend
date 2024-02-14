50b4955fdfd0fb54fc2f601da8d7c593304a7fdc
HyunSu1768 <azxcv1768@gmail.com>
2024-02-14 19:30:44 +0900

    feat :: regex 패턴 추가

--- src/main/kotlin/com/example/mergebackend/domain/deploy/presentation/DeployController.kt
+++ src/main/kotlin/com/example/mergebackend/domain/deploy/presentation/DeployController.kt
@@ -4,12 +4,15 @@ import com.example.mergebackend.domain.deploy.presentation.dto.request.CreateDep
 import com.example.mergebackend.domain.deploy.presentation.dto.response.CreateDeployResponse
 import com.example.mergebackend.domain.deploy.service.DeployService
 import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
+@Validated
 @RequestMapping("/deploy")
 @RestController
 class DeployController(
@@ -18,7 +21,7 @@ class DeployController(
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun createDeploy(
-        @RequestBody
+        @RequestBody @Valid
         createDeployRequest: CreateDeployRequest
     ): CreateDeployResponse {
         return deployService.createDeploy(createDeployRequest)
