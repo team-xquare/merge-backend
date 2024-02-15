53227a968c0de0e6d952d7bbb80df200eeda5c3d
HyunSu1768 <azxcv1768@gmail.com>
2024-02-15 20:43:45 +0900

    bug :: transactional 추가

--- src/main/kotlin/com/example/mergebackend/domain/deploy/service/impl/DeployServiceImpl.kt
+++ src/main/kotlin/com/example/mergebackend/domain/deploy/service/impl/DeployServiceImpl.kt
@@ -18,8 +18,10 @@ import com.example.mergebackend.infra.deploy.DeployClient
 import com.example.mergebackend.infra.deploy.dto.FeignCreateDeployRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
+@Transactional
 @Service
 private class DeployServiceImpl(
     private val deployRepository: DeployRepository,
